1be3f6776741e94728b7a720d518e02372f16f62
Joseph Edusei <jedusei99@gmail.com>
2020-12-04 07:11:29 +0000

    Updated App.xaml.cs

--- RealEstate.Android/MainActivity.cs
+++ RealEstate.Android/MainActivity.cs
@@ -1,15 +1,11 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace RealEstate.Droid
 {
-    [Activity(Label = "RealEstate", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "RealEstate", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -21,7 +17,7 @@ namespace RealEstate.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            LoadApplication(new App(Finish));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
--- RealEstate/App.xaml.cs
+++ RealEstate/App.xaml.cs
@@ -1,28 +1,74 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 namespace RealEstate
 {
-    public partial class App : Application
+    public enum AppStatus
     {
-        public App()
+        Starting,
+        Started,
+        Paused,
+        Stopped
+    }
+
+    public partial class App : Xamarin.Forms.Application
+    {
+        static Action _exitAction;
+        public static AppStatus Status { get; private set; }
+        public static event Action Exit;
+
+        public App(Action exitAction = null)
         {
             InitializeComponent();
+            _exitAction = exitAction ?? Current.Quit;
+
+            On<Xamarin.Forms.PlatformConfiguration.Android>()
+                .UseWindowSoftInputModeAdjust(WindowSoftInputModeAdjust.Resize);
 
             MainPage = new MainPage();
         }
 
+        public static Task NextTickAsync()
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            Device.StartTimer(TimeSpan.FromSeconds(0), () =>
+            {
+                Device.BeginInvokeOnMainThread(() => tcs.SetResult(true));
+                return false;
+            });
+
+            return tcs.Task;
+        }
+
+        public new static void Quit()
+        {
+            if (Status != AppStatus.Stopped && _exitAction != null)
+                _exitAction.Invoke();
+        }
+
         protected override void OnStart()
         {
+            Status = AppStatus.Started;
         }
 
         protected override void OnSleep()
         {
+            Status = AppStatus.Paused;
         }
 
         protected override void OnResume()
         {
+            Status = AppStatus.Started;
+        }
+
+        public void OnExit()
+        {
+            Status = AppStatus.Stopped;
+            Exit?.Invoke();
+            Exit = null;
         }
     }
 }
