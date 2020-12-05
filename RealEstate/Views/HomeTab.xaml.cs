c7d4bb7f296a77c80e3a6e82aff3f90493df8969
Joseph Edusei <jedusei99@gmail.com>
2020-12-05 01:05:41 +0000

    Home tab initial design.

---
+++ RealEstate/Views/HomeTab.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace RealEstate.Views
+{
+    public partial class HomeTab : ContentView, ITabContentView
+    {
+        public HomeTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
