cf907662fd835dcf633bf338099bcf8ba042acc7
Joseph Edusei <jedusei99@gmail.com>
2020-12-06 01:22:16 +0000

    Lock screen to portrait mode.

--- RealEstate.Android/MainActivity.cs
+++ RealEstate.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Acr.UserDialogs;
 
 namespace RealEstate.Droid
 {
-    [Activity(Label = "RealEstate", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "RealEstate", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         App _app;
