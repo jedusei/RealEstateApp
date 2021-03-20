068dc247b8514ba6e93d07d6fd1364e77cb8bc01
Joseph Edusei <jedusei99@gmail.com>
2021-03-20 17:56:38 +0000

    Completed filter modal.

--- RealEstate/Views/FilterModal.xaml.cs
+++ RealEstate/Views/FilterModal.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace RealEstate.Views
+﻿using RealEstate.ViewModels;
+
+namespace RealEstate.Views
 {
     public partial class FilterModal : BottomSheetModal
     {
         public FilterModal()
         {
             InitializeComponent();
+            BindingContext = new FilterViewModel(x => { });
         }
     }
 }
